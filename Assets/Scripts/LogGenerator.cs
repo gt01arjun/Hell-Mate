16e983f0323bf2428d86709988484fcd792bd432
Arjun <gt01arjun@thecea.ca>
2021-05-20 18:38:49 -0700

    Added Text for Initial Booster

--- Assets/Scripts/LogGenerator.cs
+++ Assets/Scripts/LogGenerator.cs
@@ -14,15 +14,25 @@ public class LogGenerator : MonoBehaviour
     private int _logYMin;
     [SerializeField]
     private int _logYMax;
+    [SerializeField]
+    private GameObject _initialBoosterText;
+
+    private int c;
 
     private void Start()
     {
+        c = 0;
         InvokeRepeating("SpawnLog", 1f, _logSpawnRate);
     }
 
     private void SpawnLog()
     {
-        Instantiate(_logPrefab, new Vector3(_logPrefab.transform.position.x, _player.transform.position.y + Random.Range(_logYMin, _logYMax), 0), _logPrefab.transform.rotation);
+        c++;
+        GameObject currentLog = Instantiate(_logPrefab, new Vector3(_logPrefab.transform.position.x, _player.transform.position.y + Random.Range(_logYMin, _logYMax), 0), _logPrefab.transform.rotation);
+        if (c == 1)
+        {
+            Instantiate(_initialBoosterText, currentLog.transform.position - new Vector3(-1.2f, 5f, 0), _initialBoosterText.transform.rotation);
+        }
     }
 
     private void Update()
